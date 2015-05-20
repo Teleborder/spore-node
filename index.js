21d1f1bfa330a4be07b09e5db22f5584cfd64e58
Trey Griffith <trey.griffith@gmail.com>
2015-05-19 21:31:10 -0700

    add a sync method for loading environment variables

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ var Spore = require('./lib/spore');
 
 Spore.spore = new Spore();
 Spore.loadEnv = Spore.spore.loadEnv.bind(Spore.spore);
+Spore.loadEnvSync = Spore.spore.loadEnvSync.bind(Spore.spore);
 
 module.exports = Spore;
